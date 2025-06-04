ad77ba22b63aca326490968dcc7f28da6d5da7a3
saikumardasi <saikumardasi21@gmail.com>
2025-06-04 22:20:05 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+package myfirstpackage;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello world");     // TODO Auto-generated method stub
+		System.out.println("This is my First Java Program");
+	}
+}
