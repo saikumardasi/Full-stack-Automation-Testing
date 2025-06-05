65aaa1b613ba789427306ef836b659ffb11dd4ee
saikumardasi <saikumardasi21@gmail.com>
2025-06-06 00:00:32 +0530

    Add files via upload

---
+++ DatatTypes.java
@@ -0,0 +1,19 @@
+package myfirstpackage;
+
+public class DatatTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int math = 70;
+		int science = 95;
+		int english =80;
+		int totalmarks = math + science + english;
+		System.out.println("The toatl marks of the student is = "+totalmarks);
+		int average = totalmarks/3;
+		System.out.println("Average marks of the student is = "+average);
+		float ave = totalmarks/3f;
+		System.out.println("Average marks of the student is = "+ave);
+	
+	}
+
+}
